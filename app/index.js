9626c33060f67be00af01a5bccdeaa942a3594a7
xHideakix <astilus1997@gmail.com>
2020-03-27 04:46:13 +0200

    add str

--- app/index.js
+++ app/index.js
@@ -1,8 +1,9 @@
 const Telegraf = require('telegraf');
-const app = new Telegraf('930886942:AAGupiVd3KdVCKsVFCzEdUxbKSAW1xE5d3I');
+const bot = new Telegraf('930886942:AAGupiVd3KdVCKsVFCzEdUxbKSAW1xE5d3I');
 
-app.hears('hi', ctx => {
-    return ctx.reply('Hey!');
+bot.hears('ping', ctx => {
+    return ctx.reply('Pong!');
 });
 
-app.startPolling();
+
+bot.startPolling();
