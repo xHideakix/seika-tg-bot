4d0319a8c09d75c6cc6789f27bdbc199bce66caf
xHideakix <astilus1997@gmail.com>
2020-03-27 11:42:35 +0200

    add change

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 const Telegraf = require('telegraf');
 const bot = new Telegraf('930886942:AAGupiVd3KdVCKsVFCzEdUxbKSAW1xE5d3I');
 
-bot.hears('ping', ctx => {
+bot.hears('ping'.toLowerCase(), ctx => {
     return ctx.reply('Pong!');
 });
 
