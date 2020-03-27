3ff2b277a663890a8353a7198c38bcc43d808b04
xHideakix <astilus1997@gmail.com>
2020-03-27 12:20:58 +0200

    change in index

--- app/index.js
+++ app/index.js
@@ -1,88 +1,46 @@
 const Telegraf = require('telegraf');
-const bot = new Telegraf('930886942:AAGupiVd3KdVCKsVFCzEdUxbKSAW1xE5d3I');
 
-bot.hears('ping'.toLowerCase(), ctx => {
-    return ctx.reply('Pong!');
+let config = {
+    "token": "930886942:AAGupiVd3KdVCKsVFCzEdUxbKSAW1xE5d3I", 
+    "admin": 319423380 // id владельца бота
+};
+
+const bot = new Telegraf(config.token, {
+
+    }
+);
+
+let replyText = {
+    "helloAdmin": "Доброе утро, создатель. Чем могу помочь? ^^",
+    "helloUser":  "Приветствую, отправьте мне сообщение. Я передам его хозяину! *-*",
+    "replyWrong": "Для ответа пользователю используйте функцию Ответить/Reply."
+};
+
+let isAdmin = (userId) => {
+    return userId == config.admin;
+};
+
+let forwardToAdmin = (ctx) => {
+    if (isAdmin(ctx.message.from.id)) {
+        ctx.reply(replyText.replyWrong);
+    } else {
+        ctx.forwardMessage(config.admin, ctx.from.id, ctx.message.id);
+    }
+};
+
+bot.start((ctx) => {
+    ctx.reply(isAdmin(ctx.message.from.id)
+        ? replyText.helloAdmin
+        : replyText.helloUser);
 });
+
 bot.on('message', (ctx) => {
-     // убеждаемся что это админ ответил на сообщение пользователя
-     if (ctx.message.reply_to_message
+    if (ctx.message.reply_to_message
         && ctx.message.reply_to_message.forward_from
         && isAdmin(ctx.message.from.id)) {
-        // кому отправляем
-        let userId = ctx.message.reply_to_message.forward_from.id;
-        //  проверяем что пришло и отправляем соответствующим методом
-        switch (ctx.updateSubTypes[0]) {
-            case 'text':
-                ctx.telegram.sendMessage(
-                    userId,
-                    ctx.message.text
-                );
-                break;
-            case 'sticker':
-                ctx.telegram.sendSticker(
-                    userId,
-                    ctx.message.sticker.file_id
-                );
-                break;
-            case 'photo':
-                let file = ctx.message.photo.length - 1;
-                ctx.telegram.sendPhoto(
-                    userId,
-                    ctx.message.photo[file].file_id,
-                    {
-                        'caption': ctx.message.caption
-                    }
-                );
-                break;
-            case 'document':
-                ctx.telegram.sendDocument(
-                    userId,
-                    ctx.message.document.file_id,
-                    {
-                        'caption': ctx.message.caption
-                    }
-                );
-                break;
-            case 'voice':
-                ctx.telegram.sendVoice(
-                    userId,
-                    ctx.message.voice.file_id,
-                    {
-                        'caption': ctx.message.caption
-                    }
-                );
-                break;
-            case 'video_note':
-                ctx.telegram.sendVideoNote(
-                    userId,
-                    ctx.message.video_note.file_id
-                );
-                break;
-            case 'video':
-                ctx.telegram.sendVideo(
-                    userId,
-                    ctx.message.video.file_id,
-                    {
-                        'caption': ctx.message.caption
-                    }
-                );
-                break;
-            case 'audio':
-                ctx.telegram.sendAudio(
-                    userId,
-                    ctx.message.audio.file_id,
-                    {
-                        'caption': ctx.message.caption
-                    }
-                );
-                break;
-            default:
-                console.log('other');
-    }
+        ctx.telegram.sendCopy(ctx.message.reply_to_message.forward_from.id, ctx.message);
     } else {
         forwardToAdmin(ctx);
     }
 });
-
-bot.startPolling();
+bot.launch();
